91a35c9f91199b9a4ae8495f78909d7cc08d5b1f
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2025-03-13 22:52:45 -0300

    ajustando layout no mobile e ajustando carrosel de imagens

--- src/app/components/carouselImage.tsx
+++ src/app/components/carouselImage.tsx
@@ -1,24 +1,45 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+import Image from "next/image"
 
-export const CarouselImage = () => {
+interface ImageProps {
+    src: string
+    alt: string
+}
+
+export const CarouselImage = ({ images }: { images: ImageProps[] }) => {
     return (
         <section
-            className="flex w-full h-[calc(100% - 80px)] sm:h-screen p-8 gap-6 items-center justify-center"
+            className="flex w-full h-[calc(100% - 80px)] sm:h-full p-8 gap-6 items-center justify-center"
         >
-            <Carousel className="flex justify-center">
-                <CarouselContent >
-                    {Array.from({ length: 5 }).map((_, index) => (
-                        <CarouselItem key={index} >
-                            <div className="p-1 w-full">
-                                <Card >
-                                    <CardContent className="w-full flex aspect-square items-center justify-center p-6">
-                                        <span className="text-4xl font-semibold">{index + 1}</span>
-                                    </CardContent>
-                                </Card>
-                            </div>
-                        </CarouselItem>
-                    ))}
+            <Carousel
+                opts={{
+                    align: "start",
+                }}
+                className="w-full max-w-sm sm:max-w-6xl"
+            >
+                <CarouselContent>
+                    {images ? (
+                        images.map((image, index) => (
+                            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                                <div className="p-1">
+                                    <Card className="p-0">
+                                        <CardContent className="flex aspect-square items-center justify-center p-0 max-h-90">
+                                            <Image
+                                                src={image.src}
+                                                alt={image.alt}
+                                                width={500}
+                                                height={500}
+                                                className="w-full h-full object-cover rounded-2xl"
+                                            />
+                                        </CardContent>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))
+                    ) : (
+                        <></>
+                    )}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,38 @@ import { CarouselImage } from "./components/carouselImage";
 import { Header } from "./components/header";
 import { Home } from "./components/home";
 
+
 export default function App() {
   return (
     <main className="w-full h-full pt-20 sm:pt-0 bg-[var(--tertiary-color)] flex flex-col items-center">
       <Header />
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-20 justify-center items-center">
         <Home />
         <About />
-        <CarouselImage />
+        <CarouselImage
+          images={[
+            {
+              src: "/images/carousel/1.jpg",
+              alt: "Imagem 1",
+            },
+            {
+              src: "/images/carousel/2.jpg",
+              alt: "Imagem 2",
+            },
+            {
+              src: "/images/carousel/3.jpg",
+              alt: "Imagem 3",
+            },
+            {
+              src: "/images/carousel/4.jpg",
+              alt: "Imagem 4",
+            },
+            {
+              src: "/images/carousel/5.jpg",
+              alt: "Imagem 5",
+            },
+          ]}
+        />
       </div>
     </main>
   )
